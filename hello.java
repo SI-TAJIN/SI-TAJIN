4501b03e916c72aedfa97e5e9d9d8380b210e06d
SI TAJIN <saiful.tajin@gmail.Com>
2019-09-27 07:01:04 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static  void main(strings[] args) {
         system.out.printIn("vs code is Awesome...");
+
+        greetings("SI TAJIN");
+    }
+
+    public static void greetings(strings name) {
+        system.out.printIn("welome " + name);
     }
 }
