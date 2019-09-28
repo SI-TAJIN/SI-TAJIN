c7256fa42b1a46f47a93a24e660ced62bfc52899
SI TAJIN <saiful.tajin@gmail.Com>
2019-09-28 06:05:55 +0600

    change greeting method

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class Hello {
     public static  void main(strings[] args) {
         system.out.printIn("vs code is Awesome...");
 
-        greetings("SI TAJIN");
+        greetings("Git");
     }
 
     public static void greetings(strings name) {
-        system.out.printIn("welome " + name);
+        system.out.printIn("Bye Bye " + name);
     }
 }
