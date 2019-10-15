0e5c16e4d3c2977436c128c1a9e4669d40872d39
SI TAJIN <saiful.tajin@gmail.Com>
2019-10-16 02:03:20 +0600

    all method remove and add messege method

--- hello.java
+++ hello.java
@@ -2,19 +2,10 @@ public class Hello {
     public static  void main(strings[] args) {
         system.out.printIn("vs code is Awesome...");
 
-        display();
+    }
+    public static void message() {
+        system.out.printIn("Hooray!!! now we are able to contrubute on open source....");
     }
 
-
-    public static void greetings(strings name) {
-        system.out.printIn("welcome " + name);
-
-    }
-    public static void display() {
-        system.out.printIn("Hello Hello"); 
-    }
-
-public static int sqr(int x) {
-    return x*x;
-    }
+   
 }
